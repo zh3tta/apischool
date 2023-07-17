da7eace23f92a7b1f2fcf4c0a474c1ef74d8bf2a
ignaciozhetta <zagu4988@gmail.com>
2023-07-17 14:43:15 -0300

    initial config

---
+++ database/migrations/2023_07_17_162425_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->integer('dni')->unique();
+            $table->string('name');
+            $table->string('address');
+            $table->string('email')->unique();
+            $table->integer('phone');
+            $table->enum('removed',['true','false'])->default('false');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
