709c86e9157646e81e51ae9d95629e8b8a87fa3a
ignaciozhetta <zagu4988@gmail.com>
2023-07-23 19:15:41 -0300

    controller and route of subject added

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -9,26 +9,43 @@ class SubjectController extends Controller
 {
     public function index()
     {
-        //
+        $subject = Subject::where('removed', '=', 'false')
+                            ->get();
+        return $subject;
     }
 
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'code'=>'required',
+            'name'=>'required',
+            'career_id'=>'required',
+            'teacher_id'=>'required'
+        ]);
+        $subject = new Subject($request->input());
+        $subject->save();
+        return $subject;
     }
 
     public function show(Subject $subject)
     {
-        //
+        $subject = Subject::where('id', $subject->id)
+                        ->where('removed', '=', 'false')
+                        ->get();
+        return $subject;
     }
 
     public function update(Request $request, Subject $subject)
     {
-        //
+        $subject->update($request->input());
+        return $subject;
     }
 
     public function destroy(Subject $subject)
     {
-        //
+        $subject->update([
+            'removed' => 'true'
+        ]);
+        return $subject;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,10 @@ use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\CareerController;
 use App\Http\Controllers\GradeController;
+use App\Http\Controllers\SubjectController;
 
 Route::apiResource('students', StudentController::class);
 Route::apiResource('teachers', TeacherController::class);
 Route::apiResource('careers', CareerController::class);
-Route::appiResource('grades', GradeController::class);
+Route::apiResource('grades', GradeController::class);
+Route::apiResource('subjects', SubjectController::class);
