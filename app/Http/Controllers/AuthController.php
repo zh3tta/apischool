948bc3065c136842a5a7d518590cfab872bfc0a8
ignaciozhetta <zagu4988@gmail.com>
2023-07-29 13:18:55 -0300

    [feat] auth with sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function create(Request $request) {
+        $rules = [
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|min:8'
+        ];
+        $validator = Validator::make($request->input(),$rules);
+        if($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()->all()
+            ],400);
+        }
+        $user = User::create([
+            'name' => $request->name,'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        return response()->json([
+            'status' => true,
+            'message' => 'User Created Successfully',
+            'token' => $user->createToken('API TOKEN')->plainTextToken
+        ],200);
+    }
+
+    public function login(Request $request){
+        $rules = [
+            'email' => 'required|string|email|max:100',
+            'password' => 'required|string'
+        ];
+        $validator = Validator::make($request->input(),$rules);
+        if($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()->all()
+            ],400);
+        }
+        if(!Auth::attempt($request->only('email','password'))){
+            return response()->json([
+                'status' => false,
+                'errors' => ['Unauthorized']
+            ],401);
+        }
+        $user = User::where('email',$request->email)->first();
+        return response()->json([
+            'status' => true,
+            'message' => 'User llogged in successfully',
+            'data' => $user,
+            'token' => $user->createToken('API TOKEN')->plainTextToken
+        ],200);
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'User logged out successfully'
+        ],200);
+    }
+}
