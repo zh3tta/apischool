e31633d175a0654dbcb121a94597ea9e96565fde
ignaciozhetta <zagu4988@gmail.com>
2023-07-29 13:37:05 -0300

    routes with auth added

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,18 @@ use App\Http\Controllers\CareerController;
 use App\Http\Controllers\GradeController;
 use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\InstitutionController;
+use App\Http\Controllers\AuthController;
 
-Route::apiResource('students', StudentController::class);
-Route::apiResource('teachers', TeacherController::class);
-Route::apiResource('careers', CareerController::class);
-Route::apiResource('grades', GradeController::class);
-Route::apiResource('subjects', SubjectController::class);
-Route::get('institution', [InstitutionController::class, 'showInstitution']);
-Route::put('institution/{institution}', [InstitutionController::class, 'update']);
+Route::post('auth/register', [AuthController::class, 'create']);
+Route::post('auth/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('students', StudentController::class);
+    Route::apiResource('teachers', TeacherController::class);
+    Route::apiResource('careers', CareerController::class);
+    Route::apiResource('grades', GradeController::class);
+    Route::apiResource('subjects', SubjectController::class);
+    Route::get('institution', [InstitutionController::class, 'showInstitution']);
+    Route::put('institution/{institution}', [InstitutionController::class, 'update']);
+    Route::get('auth/logout', [AuthController::class, 'logout']);
+});
